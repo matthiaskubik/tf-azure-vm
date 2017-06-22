d89d0cdc8ece9bd7a210da042488369213b753c9
matthiaskubik <kubik@de.ibm.com>
2017-06-22 13:04:42 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "azure_subscription_id" {
+   type = "string"
+   default = ""
+   description = "Your Azure subscription ID (optional)"
+}
+
+variable "azure_client_id" {
+   type = "string"
+   default = ""
+   description = "Your Azure client ID."
+}
+
+variable "azure_client_secret" {
+   type = "string"
+   default = ""
+   description = "Your client_secret."
+}
+
+variable "azure_tenant_id" {
+   type = "string"
+   default = ""
+   description = "Your tenant ID."
+}
